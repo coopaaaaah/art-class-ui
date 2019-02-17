12c6ff8ce46c93f8a33016e4879ea2c5d51683f1
David Cooper <dmcooper2412@gmail.com>
2019-02-17 10:12:49 -0500

    messing with align-items

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ export class App extends React.Component {
     render() {
         return (
             <div className='app'>
-                <h2>Hello World</h2>
+                <h2> Flex Reverse Row </h2>
                 <div className='flex-row'>
                     <div className='col-4'>
                         Child 1
@@ -18,6 +18,25 @@ export class App extends React.Component {
                         Child 3
                     </div>
                 </div>
+
+                <h2> Flex Reverse Column </h2>
+                <div className='flex-column'>
+                    <div className='col-4'>
+                        Child 1
+                    </div>
+                    <div className='col-4'>
+                        Child 2
+                    </div>
+                    <div className='col-4'>
+                        Child 3
+                    </div>
+                </div>
+
+                <h2>Centered Item</h2>
+                <div className="flex-row-center-single-item">
+                    <div>Centered!</div>
+                </div>
+
             </div>
         )
     }
