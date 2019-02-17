33351063ed3dc7b8a127d13af73317806d2a824c
David Cooper <dmcooper2412@gmail.com>
2019-02-17 16:07:09 -0500

    converted to sass!

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,8 @@ module.exports = options => {
           ]
         },
         {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
         },
       ]
     }
